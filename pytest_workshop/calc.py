015e8bb4a0be878818954cf041a9a037c6de2686
Dr.-Ing. Peter Kötzing <pkoetzing@online.de>
2020-09-27 22:43:18 +0200

    @pytest.mark.parametrize

--- pytest_workshop/calc.py
+++ pytest_workshop/calc.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 class Calc():
+
     def add(self, *args):
         return sum(args)
+
+    def sub(self, *args):
+        return args[0] - sum(args[1:])
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -8,6 +8,8 @@ test_pytest_workshop
 Tests for `pytest_workshop` module.
 """
 
+import pytest
+
 from pytest_workshop.calc import Calc
 
 
@@ -16,11 +18,32 @@ def test_add_two_numbers():
     assert c.add(4, 5) == 9
 
 
-def test_add_three_numbers():
-    c = Calc()
-    assert c.add(4, 5, 6) == 15
-
-
 def test_add_many_numbers():
     c = Calc()
     assert c.add(*range(100)) == 4950
+
+
+def test_sub_two_numbers():
+    c = Calc()
+    assert c.sub(10, 3) == 7
+
+
+def test_sub_many_numbers():
+    c = Calc()
+    assert c.sub(100, 11, 7, 21) == 61
+
+
+@pytest.mark.parametrize(
+    'actual, expected',
+    [
+        ('100, 90', 10),
+        ('-10, -2', -8),
+        ('0, -8', 8),
+        ('100, 11, 7, 21', 61),
+        pytest.param("100, 90", 42, marks=pytest.mark.xfail)
+    ])
+def test_sub(actual, expected):
+    '''Test "corner cases" of sub function'''
+    c = Calc()
+    act = tuple(map(int, actual.split(',')))
+    assert c.sub(*act) == expected
