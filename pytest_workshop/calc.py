b6a8a4efb7866667562851302846320c7a586bd3
Dr.-Ing. Peter Kötzing <pkoetzing@online.de>
2020-09-27 17:19:09 +0200

    test_add_three_numbers

--- pytest_workshop/calc.py
+++ pytest_workshop/calc.py
@@ -1,1 +1,4 @@
 # -*- coding: utf-8 -*-
+class Calc():
+    def add(self, a, b):
+        return 9
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -8,3 +8,14 @@ test_pytest_workshop
 Tests for `pytest_workshop` module.
 """
 
+from pytest_workshop.calc import Calc
+
+
+def test_add_two_numbers():
+    c = Calc()
+    assert c.add(4, 5) == 9
+
+
+def test_add_three_numbers():
+    c = Calc()
+    assert c.add(4, 5, 6) == 15
